b68168b08073d31151e0cd973704b4d975f91fa2
danieljuma5 <jumadaniel275@gmail.com>
2023-03-10 08:05:08 +0300

    Completetd assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+//Rewriting our for loop into a gift wrapping
+
+function writeCards(names,eventName) {
+  let messagesArray = [];
+  for (let i = names.length - 1; i >= 0; i--) {
+  
+  messagesArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+}
+console.log(messagesArray);
+}
+
+const attendees = ["Guadalupe", "Ollie", "Aki"];
+const eventName = "surprise";
+const result = writeCards(["Guadalupe", "Ollie", "Aki"],"surprise");
+
+
+//countdown function
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
